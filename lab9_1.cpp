e323605b1cbd6fe2392ec545b98b624b82da0ae1
kanthidaja <kanthida_khun@cmu..ac.th>
2024-12-13 16:11:16 +0700

    completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){	
 	int EndOfYear=1;
-	double pay,interest,Prev,Interest,Total,Newbalance=0,NewPrev=0;
+	double pay,interest,Prev,Interest,Total,Newbalance=0;
 	cout << "Enter initial loan: ";
 	cin >> Prev;
 	cout << "Enter interest rate per year (%): ";
@@ -12,9 +12,20 @@ int main(){
 	cout << "Enter amount you can pay per year: ";
 	cin >> pay;
     
+	    	cout << setw(13) << left << "EndOfYear#"; 
+	cout << setw(13) << left << "PrevBalance"; 
+	cout << setw(13) << left << "Interest"; 
+	cout << setw(13) << left << "Total";
+	cout << setw(13) << left << "Payment";
+	cout << setw(13) << left << "NewBalance";
+	cout << "\n";
+
     do{
 		Interest = (interest*Prev)/100;
 	    Total = Prev + Interest;
+		if(Total<pay){
+			pay = Total;
+		}
 	    Newbalance = Total - pay;
 
 			cout << fixed << setprecision(2); 
@@ -29,39 +40,8 @@ int main(){
 	Prev = Newbalance;
 	EndOfYear++;
 
-	}while(Newbalance>pay && Total>=pay);
+	}while(Newbalance!=0);
     
-	Interest = (interest*Prev)/100;
-	Total = Prev + Interest;
-	pay = Total;
-	Newbalance = Total-pay;
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << EndOfYear; 
-	cout << setw(13) << left << Prev;
-	cout << setw(13) << left << Interest;
-	cout << setw(13) << left << Total;
-	cout << setw(13) << left << pay;
-	cout << setw(13) << left << Newbalance;
-	cout << "\n";	
 
-
-	
-		
-
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
-	cout << setw(13) << left << "Payment";
-	cout << setw(13) << left << "NewBalance";
-	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-
-	
 	return 0;
 }
